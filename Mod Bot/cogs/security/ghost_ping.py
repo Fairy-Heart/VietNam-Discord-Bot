772a6abac884db7a5459fdbd18f45b5ae719fdc7
Fairy-Heart <mavis.ngoc.nguyen@gmail.com>
2024-06-07 11:38:33 +0700

    cập nhật file ghost_ping.py

---
+++ Mod Bot/cogs/security/ghost_ping.py
@@ -0,0 +1,29 @@
+from discord.ext import commands
+import discord
+import datetime
+from discord import app_commands
+
+class GhostPing(commands.Cog):
+    def __init__(self,  bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message: discord.Message):
+
+        if message.mentions and message.author.bot:
+            return False
+
+        if message.mentions:
+            embed = discord.Embed(
+                title = 'Đã phát hiện Ghost Ping !',
+                description = f'* Nội dung tin nhắn ban đầu :\n* `{message.content}`\n* Gửi vào lúc : `{message.created_at}`',
+                color = 0x00ffff,
+                timestamp = datetime.datetime.now()
+            )
+            embed.set_thumbnail(
+                url = f'{message.author.avatar}'
+            )
+            await message.channel.send(embed = embed)
+
+async def setup(bot):
+    await bot.add_cog(GhostPing(bot))
